31195c077f3c5a3e537ad88cf0b9f1b475490385
Amechi Egbe <egbe@usc.edu>
2014-08-20 12:58:04 -0400

    Added Grunt task

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function (grunt) {
+
+	//Project configuration
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		watch:{
+			js: {
+				files: ['Gruntfile.js', 'dist/*.js']
+			},
+			css: {
+				files: ['css/core.css']
+			}
+			tasks:['jslint','csslint']
+		},
+		
+		jshint: {
+			options: {
+				jshintrc: {
+					"node":true,
+					"curly":false,
+					"indent":4,
+					"quotmark":"single",
+					"globals": {
+						'jQuery':true
+					}
+				}
+			},
+			all: ['Gruntfile.js', 'dist/*.js']
+		}
+	});
+
+	//Loading Grunt Plugins
+	grunt.loadNpmTasks('grunt-contrib-csslint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+
+
+	//Register default task
+	grunt.registerTask('default', ['watch']);
+
+};
