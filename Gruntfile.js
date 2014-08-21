cf1c560499ea818ef288f4e74058a9fc28ef1bb8
Amechi Egbe <egbe@usc.edu>
2014-08-21 10:56:57 -0400

    grunt change

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,8 +44,10 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 
+	//Grunt Options
+	grunt.option("force", true);
 
 	//Register default task
-	grunt.registerTask('default', ["watch"], ["csslint:strict"]);
+	grunt.registerTask('default', ["csslint:strict"]);
 
 };
